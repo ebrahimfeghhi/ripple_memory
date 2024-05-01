8ea9375f2d83106ded80557f4d5325070d78cee6
ebrahimfeghhi <efeghhi@gmail.com>
2024-05-01 15:41:46 -0400

    Updated pac code

--- analysis_code/pac_analyses/.ipynb_checkpoints/comodulogram-checkpoint.py
+++ analysis_code/pac_analyses/.ipynb_checkpoints/comodulogram-checkpoint.py
@@ -6,102 +6,198 @@ from analyze_data import *
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from scipy.signal import decimate, resample
 
 from mne.time_frequency import tfr_array_morlet
 from scipy.signal import hilbert
-from load_data_numpy import load_data_np
 
-from pactools import Comodulogram, REFERENCES
-from pactools.comodulogram import read_comodulogram
-from pactools import simulate_pac
 
-print("Generating comodulogram")
-
-run_mode = int(sys.argv[1])
-
-if run_mode == 1:
-    encoding_mode = 1
-    saveName = 'encoding_'
+def remove_session_string(subject_session_elec_string):
     
-if run_mode == 2:
-    encoding_mode = 0
-    saveName = 'recall_'
+    subj_sess = subject_session_elec_string.split('_')[0]
+    subj = subj_sess.split('-')[0]
     
-dd_trials = load_data_np(encoding_mode)
-
-if encoding_mode:
+    elec = subject_session_elec_string.split('_')[1]
     
-    # relative to word onset 
-    start_roi = 300
-    end_roi = 1300
+    subj_elec = f"{subj}_{elec}"
     
-    # each trial consists of 5 sec of raw data, which starts
-    # 1.7 before word onset and ends 3.3 sec after word onset 
-    # the data is sampled at 500 Hz
-    start_time = -1700
-    end_time = 3300
+    return subj_elec
+
+def get_filtered_signal(raw_data, freq_range, start_idx, end_idx, fs):
+    
+    '''
+    :param ndarray raw_data: n_epochs x n_timepoints
+    :param list freq_range: list of lists, where each sublist is a frequency range
+    :param int start_idx, end_idx: after filtering, take only timepoints between start and end_idx
+    :param int fs: sampling frequency 
+    
+    Returns hilbert transformed signal after bandpassing. 
+    '''
+    
+    from mne.filter import filter_data
+    
+    n_frequencies = len(freq_range)
+    n_epochs = raw_data.shape[0]
+    n_points = raw_data.shape[1]
+    
+    filtered = np.zeros((n_frequencies, n_epochs, n_points),
+                        dtype=np.complex128)
+    
+    for jj, frequency in enumerate(freq_range):
+        
+        bandpassed_data = filter_data(raw_data, sfreq=fs, l_freq=frequency[0], 
+                                              h_freq=frequency[1], verbose=False)
+        filtered[jj, :] = hilbert(bandpassed_data)
+        
+    return filtered[:, :, start_idx:end_idx]
+
+def compute_pac(low_sig, high_sig, n_bins=18, min_shift=50, n_surrogates=300):
+    
+    
+    n_low = low_sig.shape[0]
+    n_high = high_sig.shape[0]
+    
+    phase_vals = np.real(np.angle(low_sig))
+    amplitude_vals = np.real(np.abs(high_sig))
+
+    # add eps so that pi is included in a bin
+    eps=1e-4
+    phase_bins = np.linspace(-np.pi, np.pi+eps, n_bins+1)
+    # get the indices of the bins to which each value in input belongs (0-18)
+    phase_preprocessed = np.digitize(phase_vals, phase_bins) - 1
+    
+    MI_vals = []
+    phase_amp_hist_storage = []
+    
+    # for each low/high frequency pair, compute MI and phase amplitude distribution
+    for low in range(n_low):
+        
+        phase = phase_preprocessed[low]
+        
+        for high in range(n_high):
+            
+            amp = amplitude_vals[high]
+            MI_surrogates, amplitude_dist_surrogates = modulation_index(phase, amp, n_surrogates, n_bins, min_shift)
+            
+            MI_vals.append(MI_surrogates)
+            phase_amp_hist_storage.append(amplitude_dist_surrogates)
+
+            
+    return MI_vals, phase_amp_hist_storage
+
+def circular_permute_within_epochs(data_arr, min_shift):
+    
+    n_epochs = data_arr.shape[0]
+    n_points = data_arr.shape[1]
+    
+    data_arr_shuffled = np.zeros_like(data_arr)
+    
+    
+    for epoch_idx in range(n_epochs):
+        
+        # circular permute each epoch
+        shift_val = np.random.randint(min_shift, n_points-min_shift)
+        data_arr_shuffled[epoch_idx] = np.hstack((data_arr[epoch_idx, shift_val:], data_arr[epoch_idx, :shift_val]))
+        
+    return data_arr_shuffled
 
     
-else:
+def modulation_index(phase_orig, amplitude, n_surrogates, n_bins, min_shift):
     
-    start_roi = -1100
-    end_roi = -100
+    from copy import deepcopy
     
-    # for recall, 
-    # each trial consists of 6 sec of data, centered around word recall 
-    start_time = -3000
-    end_time = 3000
+    MI_surrogates = []
+    amplitude_dist_surrogates = []
     
-sr_factor = 2
+    for n in range(n_surrogates+1):
+         
+        if n > 0:
+            phase = circular_permute_within_epochs(phase_orig, min_shift)
+        else:
+            phase = phase_orig
+ 
+        phase_1d = np.ravel(phase)
+        amplitude_1d = np.ravel(amplitude)
+ 
+        amplitude_dist = np.ones(n_bins)  # default is 1 to avoid log(0)
+    
+        # average across all gamma values that occur at a given theta phase bin
+        for b in np.unique(phase_1d):
+            selection = amplitude_1d[phase_1d == b]
+            amplitude_dist[b] = np.mean(selection)
 
-# convert to indices based on start time and sampling rate factor
-start_idx = int((start_roi - start_time)/sr_factor)
-end_idx = int((end_roi-start_time)/sr_factor)
-
-
-raw_data = dd_trials['raw']
-clust = dd_trials['clust_int']
-
-nonclust_idxs = np.argwhere(clust==0)
-clust_idxs = np.argwhere(clust==1)
-elec_clust_idxs = dd_trials['elec_labels'][clust_idxs]
-elec_nonclust_idxs = dd_trials['elec_labels'][nonclust_idxs]
-raw_data_clust = raw_data[clust_idxs].squeeze()
-raw_data_not_clust = raw_data[nonclust_idxs].squeeze()
-
-low_fq_range = np.linspace(2, 8, 7)
-high_fq_range = np.arange(30, 140, 10)
-fs = 500
-low_fq_width = 1.0  # Hz
-progress_bar=True
-method = 'tort'
-
-print("Creating comodulogram for clustered data")
-# only compute PAC within ROI
-mask = np.zeros_like(raw_data_clust)
-mask[:, start_idx:end_idx] = 1
-mask = mask.astype(bool)
-estimator = Comodulogram(fs=fs, low_fq_range=low_fq_range, high_fq_range=high_fq_range, 
-                         low_fq_width=low_fq_width, method=method,
-                         progress_bar=progress_bar, n_surrogates=30)
-estimator.fit(raw_data_clust, mask=mask)
-
-estimator.save(f'saved_results/comodulogram/{saveName}clustered.hdf5', overwrite=True)
-
-del estimator
-del raw_data_clust
-
-print("Creating comodulogram for non-clustered data")
-# run not clustered 
-mask = np.zeros_like(raw_data_not_clust)
-mask[:, start_idx:end_idx] = 1
-mask = mask.astype(bool)
-estimator = Comodulogram(fs=fs, low_fq_range=low_fq_range, high_fq_range=high_fq_range, 
-                         low_fq_width=low_fq_width, method=method,
-                         progress_bar=progress_bar, n_surrogates=30)
-estimator.fit(raw_data_not_clust, mask=mask)
+        # Kullback-Leibler divergence of the distribution vs uniform
+        amplitude_dist /= np.sum(amplitude_dist) # make into a pdist
+        divergence_kl = np.sum(
+            amplitude_dist * np.log(amplitude_dist * n_bins))
+        
+        # divide by log(n_bins) to bound by 1
+        MI = divergence_kl / np.log(n_bins)
+            
+        MI_surrogates.append(MI) 
+        
+        # the first value is without shuffling
+        amplitude_dist_surrogates.append(amplitude_dist)
+       
+    
+    return MI_surrogates, amplitude_dist_surrogates
 
 
-estimator.save(f'saved_results/comodulogram/{saveName}not_clustered.hdf5', overwrite=True)
+def save_MI_amplitude(subj_elec_idxs_all, raw_data, postfix_save, fs, savePath, 
+                      low_fq_range, high_fq_range, start_idx, end_idx, 
+                      subj_elec_min_epochs_condition, random_seed=42):
+    
+    '''
+    :param ndarray subj_elec_idxs_all: array of shape num_epochs, 
+            which indicates which subject+electrode combo that epoch belongs to
+            
+    :param ndarray raw_data: raw neural data, num_epochs x num_timepoints
+    
+    :param str postfix_save: MI and PAC distributions are saved as subj_elec_postfix_save
+    
+    :param int fs: sampling frequency
+    
+    :param str savePath: folder to save data
+    '''
+    
+    np.random.seed(random_seed)
+
+    for subj_elec in np.unique(subj_elec_idxs_all):
+        
+        save_dict = {}
+
+        subj_elec_idxs = np.argwhere(subj_elec_idxs_all==subj_elec).squeeze()
+        
+        num_epochs = subj_elec_idxs.shape[0] 
+        
+        if subj_elec in subj_elec_min_epochs_condition.keys():
+            
+            min_epochs = int(subj_elec_min_epochs_condition[subj_elec])
+            
+            # randomly select a subportion of epochs so that trial count
+            # is equivalent across conditions
+            if min_epochs < num_epochs:
+                print(min_epochs, num_epochs)
+                selected_epochs = np.random.randint(0, num_epochs, min_epochs)
+                subj_elec_idxs = subj_elec_idxs[selected_epochs].squeeze()
+        else:
+            
+            continue
+
+        if subj_elec_idxs.shape[0] < 10:
+            continue
+
+        raw_data_elec_subj = raw_data[subj_elec_idxs].squeeze()
+
+        low_sig = get_filtered_signal(raw_data_elec_subj, low_fq_range, start_idx, end_idx, fs=fs)
+        high_sig = get_filtered_signal(raw_data_elec_subj, high_fq_range, start_idx, end_idx, fs=fs)
+ 
+        MI, pac_hist = compute_pac(low_sig, high_sig, n_bins=18, min_shift=50, n_surrogates=300)
+
+        save_dict['MI'] = MI
+        save_dict['pac_hist'] = pac_hist
+
+        np.savez(f'{savePath}{subj_elec}_{postfix_save}', **save_dict)
+        
+        print("SAVED DATA")
+
---
+++ analysis_code/pac_analyses/run_comod.py
@@ -0,0 +1,95 @@
+import numpy as np
+import sys 
+sys.path.append('/home1/efeghhi/ripple_memory/analysis_code/')
+from load_data import *
+from analyze_data import *
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.signal import decimate, resample
+
+from comodulogram import save_MI_amplitude, remove_session_string
+import numpy as np
+from load_data_numpy import load_data_np
+
+
+run_mode = 1
+
+if run_mode == 1:
+    encoding_mode = 1
+    saveName = 'encoding_'
+    
+if run_mode == 2:
+    encoding_mode = 0
+    saveName = 'recall_'
+    
+dd_trials = load_data_np(encoding_mode)
+
+if encoding_mode:
+    # relative to word onset 
+    start_roi = 300
+    end_roi = 1300
+    
+    # each trial consists of 5 sec of raw data, which starts
+    # 1.7 before word onset and ends 3.3 sec after word onset 
+    # the data is sampled at 500 Hz
+    start_time = -1700
+    end_time = 3300
+
+else:
+    
+    start_roi = -1100
+    end_roi = -100
+    
+    # for recall, 
+    # each trial consists of 6 sec of data, centered around word recall 
+    start_time = -3000
+    end_time = 3000
+    
+sr_factor = 2
+
+# convert to indices based on start time and sampling rate factor
+start_idx = int((start_roi - start_time)/sr_factor)
+end_idx = int((end_roi-start_time)/sr_factor)
+
+print(start_idx, end_idx)
+
+raw_data = dd_trials['raw']
+clust = dd_trials['clust_int']
+correct = dd_trials['correct']
+
+nonclust_idxs = np.argwhere(clust==0)
+clust_idxs = np.argwhere(clust==1)
+incorrect_idxs = np.argwhere(correct==0)
+
+subj_elec_labels = np.array([remove_session_string(x) for x in dd_trials['elec_labels']])
+
+subj_elec_clust_idxs = subj_elec_labels[clust_idxs].squeeze()
+subj_elec_nonclust_idxs = subj_elec_labels[nonclust_idxs].squeeze()
+subj_elec_incorrect_idxs = subj_elec_labels[incorrect_idxs].squeeze()
+
+subj_elec_min_trials_condition = dict(np.load("/home1/efeghhi/ripple_memory/analysis_code/pac_analyses/subj_elec_min_trials_condition.npz"))
+
+raw_data_clust = raw_data[clust_idxs].squeeze()
+raw_data_not_clust = raw_data[nonclust_idxs].squeeze()
+raw_data_incorrect = raw_data[incorrect_idxs].squeeze()
+    
+savePath = '/home1/efeghhi/ripple_memory/analysis_code/pac_analyses/saved_results/comodulogram/'
+low_fq_range = [[2,5],[6,10]]
+high_fq_range = [[30,75], [80,120], [125,170]]
+fs = 500
+
+behavorial_mode = int(sys.argv[1])
+
+if behavorial_mode == 0:
+    print("RUNNING CLUSTERED")
+    save_MI_amplitude(subj_elec_clust_idxs, raw_data_clust, 'clust', fs, savePath, 
+                  low_fq_range, high_fq_range, start_idx, end_idx, subj_elec_min_trials_condition)
+if behavorial_mode == 1:
+    print("RUNNING NOT CLUSTERED")
+    save_MI_amplitude(subj_elec_nonclust_idxs, raw_data_not_clust, 'not_clust', fs, savePath, 
+                      low_fq_range, high_fq_range, start_idx, end_idx, subj_elec_min_trials_condition)
+if behavorial_mode == 2:
+    print("RUNNING NOT CORRECT")
+    save_MI_amplitude(subj_elec_incorrect_idxs, raw_data_incorrect, 'not_recalled', fs, savePath, 
+                       low_fq_range, high_fq_range, start_idx, end_idx, subj_elec_min_trials_condition)
